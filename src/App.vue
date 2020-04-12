81a06475e0274be6a2ccdb640b931b90887c3792
wangminwu <wangminwu@zuoyebang.com>
2020-04-12 15:55:44 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
+html,body,#app{
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="home">
+      <img src="../assets/logo.png" alt="logo">
+      
   </div>
 </template>
 
 <script>
-
+import lottie from 'lottie-web';
 export default {
   name: 'Home',
+    data(){
+        return{
+            lottie:{},  
+        }
+    },
+    methods:{
+        suspendFun:function(){
+            this.lottie.pause();
+        },
+        startFun:function(){
+            this.lottie.play()
+        }
+    },
+    mounted(){
+        this.lottie = lottie.loadAnimation({
+            container: document.getElementById('lottie_box'),
+            renderer: 'svg',
+            loop: true,
+            path: 'https://labs.nearpod.com/bodymovin/demo/markus/halloween/markus.json'
+        })
+    }
 }
 </script>
+<style lang="less">
+.home{
+  height: 100%;
+  background: #f66;
+}
+</style>
